03b358a4b5f9fa35b2c4e329eb01563b04fa7b8b
joshua <spasnof@gmail.com>
2018-08-12 20:32:39 -0700

    Saving work in progress. Kinda dirty right now, need to address some issue of adding nodes clearing the GUI.

--- wonderful/schema.sql
+++ wonderful/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS tables;
+DROP TABLE IF EXISTS edges;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,8 +17,29 @@ CREATE TABLE tables (
   FOREIGN KEY (owner_id) REFERENCES user (id)
 );
 
+CREATE TABLE edges (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  from_table_id INTEGER ,
+  to_table_id INTEGER,
+  created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  description TEXT NOT NULL,
+  FOREIGN KEY (from_table_id) REFERENCES tables (id),
+  FOREIGN KEY (to_table_id) REFERENCES tables (id)
+);
+
+
+
 INSERT INTO user VALUES(1,'jp','pbkdf2:sha256:50000$jj2lNTIL$31a1a03c4e693f6a66de462ea214c4a273157703004903eca3ce0ec25a9acdc0');
 
-INSERT INTO tables(owner_id, object, description) VALUES (1,'foo', 'foo desc');
-INSERT INTO tables(owner_id, object, description) VALUES (1,'foo2', 'foo desc3');
-INSERT INTO tables(owner_id, object, description) VALUES (1,'foo2', 'foo desc3');
+INSERT INTO tables(owner_id, object, description) VALUES (1,'foo.telemetry', 'button click telemetry'); --id = 1
+INSERT INTO tables(owner_id, object, description) VALUES (1,'sys.calendar', 'business calendar'); --id = 2
+INSERT INTO tables(owner_id, object, description) VALUES (1,'bar.invoices', 'invoices'); --id = 3
+INSERT INTO tables(owner_id, object, description) VALUES (1,'bar.invoice_summary', 'invoices summarized'); --id = 4
+INSERT INTO tables(owner_id, object, description) VALUES (1,'kpi.kpi', 'kpis'); --id = 5
+
+INSERT INTO edges(from_table_id, to_table_id, description) VALUES (2,1, 'button click telemetry depends on business calendar');
+INSERT INTO edges(from_table_id, to_table_id, description) VALUES (2,3, 'invoices rely on business calendar');
+INSERT INTO edges(from_table_id, to_table_id, description) VALUES (3,4, 'invoice summaries rely on invoices ');
+INSERT INTO edges(from_table_id, to_table_id, description) VALUES (4,5, 'kpis requires invoices');
+INSERT INTO edges(from_table_id, to_table_id, description) VALUES (1,5, 'kpis requires telemetry');
+
--- wonderful/tables.py
+++ wonderful/tables.py
@@ -1,5 +1,5 @@
 from flask import (
-    Blueprint, flash, g, redirect, render_template, request, url_for
+    Blueprint, flash, g, redirect, render_template, request, url_for, jsonify
 )
 from werkzeug.exceptions import abort
 
@@ -7,6 +7,7 @@ from wonderful.auth import login_required
 from wonderful.db import get_db
 
 bp = Blueprint('tables', __name__)
+
 
 @bp.route('/')
 def index():
@@ -16,8 +17,19 @@ def index():
         ' FROM tables t JOIN user u ON t.owner_id = u.id'
         ' ORDER BY created DESC'
     ).fetchall()
-    return render_template('tables/index.html', tables=meta_tables)
+    meta_edges = db.execute(
+        'SELECT e.from_table_id, e.to_table_id'
+        ' FROM edges e'
+    )
+    return render_template('tables/index.html', tables=meta_tables, edges=meta_edges)
+
+
+@bp.route('/_add_numbers', methods=['GET', 'POST'])
+def add_numbers():
+
+    c = request.args.get('c')
+    print(c)
+    return jsonify(result=c)
 
 # TODO have javascript pass back to db backend a frontend added db
 # TODO add edge support for DB
-

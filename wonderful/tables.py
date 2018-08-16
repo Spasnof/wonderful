4caa0032c889b4207e7845d35d1733115a6b28fe
joshua <spasnof@gmail.com>
2018-08-16 07:36:59 -0700

    - work in progress implementing the have services you don't own shown on click one layer at a time.

--- wonderful/tables.py
+++ wonderful/tables.py
@@ -8,7 +8,6 @@ from werkzeug.exceptions import abort
 from wonderful.auth import login_required
 from wonderful.db import get_db
 
-
 bp = Blueprint('tables', __name__)
 
 
@@ -20,6 +19,7 @@ def get_meta_tables(db, uid):
         ' ORDER BY created DESC'
     ).fetchall()
 
+
 def get_edges(db):
     return db.execute(
         'SELECT e.id, e.from_table_id, e.to_table_id, e.description'
@@ -27,15 +27,16 @@ def get_edges(db):
         ' WHERE e.visible = 1'
     ).fetchall()
 
+
 @bp.route('/')
 def index():
     """render main page"""
     db = get_db()
     user_id = session.get('user_id')
     if not (user_id):
-        return  render_template('base.html')
+        return render_template('base.html')
     print(f'user id : {user_id} is logging in.')
-    meta_tables = get_meta_tables(db,user_id)
+    meta_tables = get_meta_tables(db, user_id)
     meta_edges = get_edges(db)
     return render_template('tables/index.html', tables=meta_tables, edges=meta_edges)
 
@@ -61,7 +62,7 @@ def update_node():
     node_label = request.args.get('node_label')
     node_description = request.args.get('node_description')
     node_visible = request.args.get('node_visible')
-    #TODO add support to change ownership at some point
+    # TODO add support to change ownership at some point
     node_update = (node_visible, node_label, node_description, node_id)
     # FIXME the jinja template seems to escape the object or description wierdly adding extra quotes.
     db.execute('UPDATE tables SET visible = ?, object = ?, description = ? WHERE id = ?;', node_update)
@@ -95,14 +96,47 @@ def update_edge():
     edge_description = request.args.get('edge_description')
     edge_visible = request.args.get('edge_visible')
     update_edge = (edge_visible, from_id, to_id, edge_description, edge_id)
-    db.execute('UPDATE edges SET visible = ? , from_table_id = ? , to_table_id = ? , description = ? WHERE id = ? ;', update_edge)
+    db.execute('UPDATE edges SET visible = ? , from_table_id = ? , to_table_id = ? , description = ? WHERE id = ? ;',
+               update_edge)
     db.commit()
     return 'success'
 
 
+@bp.route('/_get_child_parent_edges', methods=['GET', 'POST'])
+def get_child_parent_edges():
+    """method to get edge nodes"""
+    db = get_db()
+    node_id = request.args.get('node_id')
+    connected_node_ids = request.args.get('connected_node_ids')
+    # diff the two edges
+    sql = '''
+    SELECT t.id, t.object, t.description
+    FROM tables t
+    WHERE t.id in 
+    (
+    -- parents that that the node id depends on
+    SELECT e1.from_table_id
+    FROM edges e1 
+    WHERE e1.to_table_id = ? 
+    
+    UNION 
+    -- children that depend on the node id
+    SELECT e2.to_table_id
+    FROm edges e2
+    WHERE e2.from_table_id = ?
+    );
+    '''
+    db.row_factory = None
+    all_connected_node_ids = db.execute(sql, (node_id, node_id)).fetchall()
+    db.commit()
+    nodes_json = jsonify(all_connected_node_ids)
+    print(request.args.get('connected_node_ids'))
+    return nodes_json
 
-#TODO have the data dictionary only show services you own
-#TODO have services you don't own displayed on click, only add one layer at a time.
-#TODO add unit tests
-#TODO remove function calls in html button elements.
-#TODO readd keybinds once you understand how they work.
+
+# TODO have the data dictionary only show services you own
+# TODO have services you don't own displayed on click, only add one layer at a time.
+# TODO have services you don't own displayed, perhaps a search?
+# TODO add unit tests
+# TODO remove function calls in html button elements.
+# TODO readd keybinds once you understand how they work.
